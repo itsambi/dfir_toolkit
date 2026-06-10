29da461a01ce0c095f9e0cc8e68cf5d6220cd2da
itsambi <ambika.dayma2301@gmail.com>
2026-06-10 09:23:59 +0530

    Forensic Timeline updation

--- dfirtoolkit/timeline/timeline_engine.py
+++ dfirtoolkit/timeline/timeline_engine.py
@@ -8,36 +8,68 @@ def build_timeline(metadata_results, history_df, downloads_df):
     timeline.append({
         "Event Type": "File Created",
         "Timestamp": metadata_results.get("Created Time"),
-        "Details": metadata_results.get("File Name")
+        "Details": metadata_results.get("File Name"),
+        "Severity": "LOW"
     })
 
     timeline.append({
         "Event Type": "File Modified",
         "Timestamp": metadata_results.get("Modified Time"),
-        "Details": metadata_results.get("File Name")
+        "Details": metadata_results.get("File Name"),
+        "Severity": "LOW"
     })
 
     # Browser history events
-    if isinstance(history_df, pd.DataFrame):
+    for _, row in history_df.head(5).iterrows():
 
-        for _, row in history_df.head(5).iterrows():
+        severity = "LOW"
 
-            timeline.append({
-                "Event Type": "Browser Visit",
-                "Timestamp": row["last_visit_time"],
-                "Details": row["url"]
-            })
+        url = str(row["url"]).lower()
+
+        suspicious_keywords = [
+            "mega",
+            "pastebin",
+            "anonfiles",
+            "temp-mail",
+            "dark"
+        ]
+
+        for keyword in suspicious_keywords:
+            if keyword in url:
+                severity = "HIGH"
+
+        timeline.append({
+            "Event Type": "Browser Visit",
+            "Timestamp": row["last_visit_time"],
+            "Details": row["url"],
+            "Severity": severity
+        })
 
     # Download events
-    if isinstance(downloads_df, pd.DataFrame):
+    for _, row in downloads_df.head(5).iterrows():
 
-        for _, row in downloads_df.head(5).iterrows():
+        severity = "LOW"
 
-            timeline.append({
-                "Event Type": "Download",
-                "Timestamp": row["start_time"],
-                "Details": row["target_path"]
-            })
+        target = str(row["target_path"]).lower()
+
+        suspicious_extensions = [
+            ".exe",
+            ".bat",
+            ".cmd",
+            ".ps1",
+            ".scr"
+        ]
+
+        for ext in suspicious_extensions:
+            if target.endswith(ext):
+                severity = "HIGH"
+
+        timeline.append({
+            "Event Type": "Download",
+            "Timestamp": row["start_time"],
+            "Details": row["target_path"],
+            "Severity": severity
+        })
 
     timeline_df = pd.DataFrame(timeline)
 
